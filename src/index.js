40c0048f9a7732a25acdcf5f3d42da5257c93601
Dominic Taylor & lou <pair+dominic-taylor.louise-r-blue@devbootcamp.com>
2016-03-18 14:15:42 +1300

    blah

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ var h = require('hyperscript')
 var request = require('superagent')
 
 request
-  .get("http://localhost:3000/meals")
+  .get("http://localhost:3000/v1/meals")
   .end(function(err,res){
     var meals = res.body
+    console.log(meals, " yip", typeof meals)
+    $('#meals').append(h('p', {}, meals.meals[0].name))
     for (var i = 0; i < meals.length; i++){
-      $('#meal-list').append(h(input), meals[i].name, {type: 'checkbox', name: 'meal', value: meals[i].id})
+      $('#meal-list').append(h('input', {type: 'checkbox', name: 'meal', value: meals.meals[i].id}, meals.meals[i].name))
     }
   })
